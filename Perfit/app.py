7ff50d4be194449a9e9c7d1bcd1e36fed4e3413e
[strong1133] <strong1133@naver.com>
2021-09-02 00:11:19 +0900

    DB Set

--- Perfit/app.py
+++ Perfit/app.py
@@ -1,17 +1,20 @@
-## URL 별로 함수명이 같거나,
-## route('/') 등의 주소가 같으면 안됩니다.
-
+from flask import Flask, render_template
 from pymongo import MongoClient
 
-from flask import Flask, render_template
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.perf_it
 
-@app.route('api/list', methods=['GET'])
-def home():
-   return render_template('index.html')
 
-doc = {'question':'answer'}
-db.users.insert_one(doc)
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+# doc = {'question': 'answer'}
+# db.users.insert_one(doc)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True, use_reloader=False)
